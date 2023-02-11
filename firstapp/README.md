9cbdc058f8979059462389277bd8da66a195792d
Pride <xixipepperd@gmail.com>
2023-02-11 17:47:40 -0500

    firstapp commit

---
+++ firstapp/README.md
@@ -0,0 +1,7 @@
+# CS374: Intro to Mobile App Development
+
+## Progress
+
+Currently, as of now, I have installed Android Studio SDK on my system, and created my first "application".
+- Pride was here
+
---
+++ firstapp/app/src/main/java/me/pride/cs374/firstapp/MainActivity.java
@@ -0,0 +1,13 @@
+package me.pride.cs374.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
